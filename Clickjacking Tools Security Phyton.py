bdbaa11097ae636c0951a8c7f389346960875e7e
AnandaRauf <uzumaki.nanda@rocketmail.com>
2018-12-18 08:07:20 +0700

    Coded by Ananda Rauf Maududi Copyright 18 December ©2018 Dont Copies and Modified Script You can IT Law in all Country

---
+++ Clickjacking Tools Security Phyton.py
@@ -0,0 +1,22 @@
+#CodedbyAnandaRaufMaududi
+#Cooyright18December©2018
+#Dontcopiesthisscriptandmodified!
+#Saveyourfileorscriptbelow
+#ChangeTargetSitetoYourTarget
+#CreateFolderinhtdocsfileInstallXamppandLaunchXampp
+#!/C/Xampp/htcdocs/yourscript
+
+print "Content-type:text/html\r\n\r\n"
+
+print "<html>"
+print "<head>"
+print "<title>Clickjacking script coded by Ananda Rauf Maududi</title>"
+print "</head>"
+print "<body>"
+print "<div>"
+print "<font color="Red>Clickjacking Penetration Testing Web Security</font color>"
+print "<iframe src="http://www.target.site" width="500" height="500"></iframe>"
+print "</div>"
+print "</body>"
+print "</html>"
+
